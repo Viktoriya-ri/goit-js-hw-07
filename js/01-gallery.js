75b44283bcc7af10820da858420412941c383de5
Вікторія <viktoriya23071@gmail.com>
2023-07-20 23:31:00 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,6 +2,49 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
-const gallerys = document.querySelector('.gallery');
-const itemsMarkup = 
-console.log(gallerys);
+
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+galleryContainer.addEventListener('click', onImgClick);
+
+function createGalleryItemsMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    }).join('');
+}
+
+const instance = basicLightbox.create(`<img width="1280" height="auto" src="">`,
+    {
+        onShow: (instance) => {
+            window.addEventListener('keydown', onEscKeyPress);
+        },
+        onClose: (instence) => {
+            window.removeEventListener('keydown', onEscKeyPress);
+        },
+    });
+
+function onImgClick(evt) {
+    evt.preventDefault();
+    const datasetSource = evt.target.dataset.source;
+    if (!datasetSource) 
+        return;
+    instance.element().querySelector('img').src = datasetSource;
+    instance.show();
+}
+    
+function onEscKeyPress(evt) {
+    if (evt.code !== 'Escape') 
+        return;
+    instance.close();
+}
